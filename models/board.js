de9c1016d2708bd2a3f8f90f5e00b4b3e856fbad
tankiet99 <45821976+tankiet99@users.noreply.github.com>
2020-12-15 08:52:02 +0700

    thêm field ở Board modal, thêm service cho route Board

--- models/board.js
+++ models/board.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BoardSchema = new Schema({
+    code: {
+        type: Number
+    },
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    },
     id_user1: {
         type: String
     },
